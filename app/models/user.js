6f2a9f4b8f75a7018480595f11a8c9180a84decb
mohitkumaris <mohitkumaris@gmail.com>
2017-12-06 17:10:38 +0530

    login and user

--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,37 @@
 /**
  * Created by Mohit Kumar on 11/24/2017.
  */
+var mongoose=require("mongoose");
+
+var bcrypt=require("bcrypt-nodejs"); //
+
+// define the schema for user model
+
+var userSchema=mongoose.Schema({
+
+    local: {
+        email:string,
+        password:string
+    },
+    facebook:{
+        email:string,
+        id:string,
+        token:string,
+        name:string
+    }
+});
+
+//generating a hash
+
+userSchema.methods.generateHash=function (password) {
+
+    return bcrypt.hashSync(password,bcrypt.genSaltSync(8),null);
+}
+
+userSchema.methods.validPassword=function (password) {
+
+    return bcrypt.compareSync(password,this.local.password);
+}
+
+// create the model for users and expose it to our app
+module.exports=mongoose.model('User',userSchema);
