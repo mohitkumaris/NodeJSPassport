f63df3ce3abe789caefda8cc497141e1e395b3c9
mohitkumaris <mohitkumaris@gmail.com>
2017-12-06 12:43:14 +0530

    basic setup done

--- app/models/routes.js
+++ app/models/routes.js
@@ -1,3 +1,55 @@
 /**
  * Created by Mohit Kumar on 11/24/2017.
  */
+module.exports=function (app,passport) {
+
+    app.get('/',function (req,res) {
+        res.render('index.ejs');
+    });
+
+    app.get('/login',function (req,res) {
+
+        res.render('login.ejs',{message:req.flash('loginmessage')});
+    });
+
+    // sign up
+
+    app.get('/signup',function (req,res) {
+
+        res.render('signup.ejs',{message:req.flash('signupmessage')});
+    });
+
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+
+    app.get('/profile',function (req,res) {
+
+        res.render('profile.ejs',{
+            user:req.user
+        });
+    });
+
+    app.get('/logout',function (req,res) {
+
+        req.logout();
+
+        res.redirect('/')
+    });
+
+    // route middleware to make sure a user is logged in
+    function isloggedIn(req,res,next) {
+
+        if(req.isAuthenticated()){
+
+            next();
+        }
+
+        res.redirect('/');
+    }
+
+
+}
--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 /**
  * Created by Mohit Kumar on 11/24/2017.
  */
-var configDB={
+module.exports={
     url:'mongodb://192.168.108.181:27017'
-}
+};
